d9560c7a19e8c3aa230ce4ccad34b3a3ca64c905
Marco Grier <mlgrier@gmail.com>
2017-07-30 10:28:02 -0500

    updated to add the shake function

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -20,7 +20,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateDiceImages()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -30,6 +32,11 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
+        updateDiceImages()
+    }
+    
+    func updateDiceImages() {
+        
         // this piece of code is makes a random number
         randomDiceIndex1 = Int(arc4random_uniform(6))
         randomDiceIndex2 = Int(arc4random_uniform(6))
@@ -37,6 +44,14 @@ class ViewController: UIViewController {
         //this takes the random number and picks the dice
         diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
         diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+        
+    }
+    
+    //this func is to shake the phone and then update the dice 
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        
+        updateDiceImages()
+        
     }
 
 }
